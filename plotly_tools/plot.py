d63b4097c2d13993f450632554f837b9f80f4342
Cristiano Aguiar <hello@cristiano.io>
2020-03-17 14:11:31 -0400

    feat: plot_scatter function

--- plotly_tools/plot.py
+++ plotly_tools/plot.py
@@ -21,12 +21,15 @@ def __add_trace_to_fig(fig, row, col, trace, secondary_y):
     return fig
 
 
-def build_trace(X, y, name, fill=None):
+def build_trace(X, y, name, fill=None, mode="lines", text=None, extra_args={}):
     return go.Scatter(
         x=X,
         y=y,
         name=name,
-        fill=fill
+        fill=fill,
+        mode=mode,
+        text=text,
+        **extra_args
     )
 
 
@@ -42,6 +45,54 @@ def list_traces_to_input(traces_list, mode="alone", trace_secondary=None):
         return [(trace, None) for trace in traces_list]
     if mode == "repeat_sec":
         return [(trace, trace_secondary) for trace in traces_list]
+
+
+def plot_scatter(
+    trace_list,
+    title=None,
+    identity=False,
+    x_name=None,
+    y_name=None,
+    height=800
+):
+    fig = go.Figure()
+    trace_list = __trace_or_list(trace_list)
+
+    max_xy = -1e10
+    min_xy = 1e10
+    for trace in trace_list:
+        max_xy = max(max(trace.x), max(trace.y), max_xy)
+        min_xy = min(min(trace.x), min(trace.y), min_xy)
+        fig.add_trace(trace)
+
+    min_xy = min(min_xy, 0.0)
+    points = [min_xy, max_xy]
+    trace_id = build_trace(
+        points,
+        points,
+        "Identity",
+        extra_args={
+            "line": {
+                "color": "black",
+                "dash": "dash"
+            }
+        })
+
+    fig.add_trace(trace_id)
+    fig.update_xaxes(title=x_name)
+    fig.update_yaxes(title=y_name)
+
+    fig.update_layout(
+        height=height,
+        width=height,
+        title=go.layout.Title(
+            text=title,
+            xref="paper",
+            x=0
+        )
+    )
+
+    return fig
 
 
 def plot_traces(trace_list, title=None, extra_args={}, height_factor=200):
